b241a9034cc3851ae9293d704fa50414a781b603
Mohan Ram S <mohanramswl@gmail.com>
2024-01-25 09:51:07 +0530

    ArmyGame

---
+++ 24-01-2024/ArmyGame.py
@@ -0,0 +1,18 @@
+def gameWithCells(n, m):
+    # Write your code here
+    return (m//2+m%2)*(n//2+n%2)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = gameWithCells(n, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
